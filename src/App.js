e022c8e40e47dff6a7747298eec5b2c2d3c38c96
Joe Wolfe <joe.wolfe21@gmail.com>
2021-01-07 11:45:34 -0500

    starting point for a single act matrix question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import questionOne from './images/act-matrix.png';
 import './App.css';
+
+const Header = () => <h2 className="header">Let's Matrix</h2>
+
+const ActSection = () => {
+  const [comment, setComment] = useState('')
+
+  const handleUpdateComment = (event) => {
+    setComment(event.target.value)
+  }
+
+  return (
+    <div className="act-section">
+      <img src={questionOne} className="act-question" alt="act matrix question" />
+      <textarea className="act-answer" placeholder="starting writing..." value={comment} onChange={handleUpdateComment} />
+      <div className="btn-group">
+        <button className="act-btn">Previous</button>
+        <button className="act-btn">Next</button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <ActSection />
     </div>
   );
 }
