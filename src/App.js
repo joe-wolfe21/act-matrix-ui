07a4f82cdcd56f4a6645b5719202875076df8720
Joe Wolfe <joe.wolfe21@gmail.com>
2021-01-07 13:16:59 -0500

    add all matrix questions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import { useState } from 'react'
-import questionOne from './images/act-matrix.png';
+
+import questionOne from './images/act-matrix-q1.png';
+import questionTwo from './images/act-matrix-q2.png';
+import questionThree from './images/act-matrix-q3.png';
+import questionFour from './images/act-matrix-q4.png';
+import questionFive from './images/act-matrix-q5.png';
 import './App.css';
+
+const questionImgs = [questionOne, questionTwo, questionThree, questionFour, questionFive]
 
 const Header = () => <h2 className="header">Let's Matrix</h2>
 
-const ActSection = () => {
-  const [comment, setComment] = useState('')
+const ActQuestion = ({questionImg}) => {
+  return (
+    <img src={questionImg} className="act-question" alt="act matrix question" />
+  )
+}
 
-  const handleUpdateComment = (event) => {
-    setComment(event.target.value)
+const ActSection = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [answers, setAnswers] = useState(['', '', '', '', ''])
+
+  const handleUpdateAnswer = (event) => {
+    const updatedAnswers = [...answers]
+    updatedAnswers[currentQuestion] = event.target.value
+
+    setAnswers(updatedAnswers)
   }
+
+  const handlePreviousClick = () => {
+    setCurrentQuestion(currentQuestion - 1)
+  }
+
+  const handleNextClick = () => {
+    setCurrentQuestion(currentQuestion + 1)
+  }
+
+  const handleSubmitClick = (event) => {
+    console.log("Submitted matrix with the following answers:", answers)
+  }
+
+  const questionImg = questionImgs[currentQuestion]
+  const answer = answers[currentQuestion]
 
   return (
     <div className="act-section">
-      <img src={questionOne} className="act-question" alt="act matrix question" />
-      <textarea className="act-answer" placeholder="starting writing..." value={comment} onChange={handleUpdateComment} />
+      <ActQuestion questionImg={questionImg} />
+      <textarea className="act-answer" placeholder="starting writing..." value={answer} onChange={handleUpdateAnswer} />
       <div className="btn-group">
-        <button className="act-btn">Previous</button>
-        <button className="act-btn">Next</button>
+        {currentQuestion > 0 && <button className="act-btn" onClick={handlePreviousClick}>Previous</button>}
+        {currentQuestion === (questionImgs.length - 1) ? <button className="act-btn" onClick={handleSubmitClick}>Submit</button> : <button className="act-btn" onClick={handleNextClick}>Next</button>}
       </div>
     </div>
   )
